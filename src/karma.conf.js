3ee8a69f5fb23f3f2be10d833ca27451d924e4b2
Adam Urbanowicz <aurbanow@pjwstk.edu.pl>
2018-12-16 11:08:46 +0100

    fix karma configuration

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,13 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['Chrome','ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['ChromeHeadless'],
     customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
+     ChromeHeadlessNoSandbox: {
+       base: 'ChromeHeadless',
+       flags: ['--no-sandbox']
+     }
+   },
     singleRun: true
   });
 };
